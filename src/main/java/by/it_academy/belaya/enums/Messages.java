6573a5cff50b50356ecc1cfb69723f75852840b8
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-03-12 16:59:33 +0300

    rename enum

--- src/main/java/by/it_academy/belaya/enums/Messages.java
+++ src/main/java/by/it_academy/belaya/enums/Messages.java
@@ -1,17 +1,18 @@
 package by.it_academy.belaya.enums;
 
-public enum SearchMessages {
+public enum Messages {
     NO_RESULTS("Простите, по вашему запросу товаров сейчас нет."),
     PAGE_NOT_FOUND("Такой страницы не существует"),
     INVALID_SEARCH_QUERY("По вашему запросу товаров сейчас нет. Но мы нашли"),
     NO_ACCOUNT_WITH_SUCH_EMAIL("We can't find the account with this email. Try entering a different one or logging in with your phone number."),
     BLANK_EMAIL("Fill in your e-mail"),
     INCORRECT_MAIL_FORMAT("Incorrect mail format"),
-    INCORRECT_PHONE_FORMAT("Incorrect phone format");
+    INCORRECT_PHONE_FORMAT("Incorrect phone format"),
+    ERROR_HAPPENED_MESSAGE("Простите, произошла ошибка. Попробуйте обновить страницу или вернуться на шаг назад.");
 
     private final String message;
 
-    SearchMessages(String message) {
+    Messages(String message) {
         this.message = message;
     }
 
