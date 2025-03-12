a5fa8b42e610bc04d151147c3c740acfe547417a
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-03-12 20:10:32 +0300

    refactor SearchAPITests - remove steps to class Steps

--- src/test/java/search/api/OzonSearchPositiveAPITest.java
+++ src/test/java/search/api/OzonSearchPositiveAPITest.java
@@ -1,30 +1,22 @@
 package search.api;
 
+import by.it_academy.belaya.utils.Steps;
 import by.it_academy.belaya.utils.ApiResponseParser;
-import by.it_academy.belaya.utils.BrowserConfigLoader;
 import by.it_academy.belaya.utils.Endpoints;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 
 
 public class OzonSearchPositiveAPITest {
 
-    private static Map<String, String> headers;
-    private static Map<String, String> cookies;
     private static final Logger logger = LogManager.getLogger();
 
     @BeforeAll
     public static void beforeAll() throws IOException {
-        headers = BrowserConfigLoader.loadHeaders();
-        cookies = BrowserConfigLoader.loadCookies();
+        Steps.loadHeadersAndCookies();
     }
 
     @BeforeEach
@@ -35,62 +27,29 @@ public class OzonSearchPositiveAPITest {
     @Test
     @DisplayName("Search by valid product article - test article button")
     public void testSearchByValidProductArticle() {
-        String expected = Endpoints.VALID_ARTICLE;
-        String responseBody = given()
-                .headers(headers)
-                .cookies(cookies)
-                .when()
-                .get(Endpoints.getSearchUrl(Endpoints.SEARCH_BY_VALID_ARTICLE))
-                .then()
-                .statusCode(200)
-                .extract()
-                .body()
-                .asString();
-        String actualText = ApiResponseParser.getArticleFromResponse(responseBody);
-
-        assertThat(actualText, containsString(expected));
+        Steps.runSearchTest(Endpoints.SEARCH_BY_VALID_ARTICLE,
+                Endpoints.VALID_ARTICLE,
+                ApiResponseParser::getArticleFromResponse);
     }
 
     @Test
     @DisplayName("Search by valid product name - test result header")
     public void testSearchByValidProductNameHeader() {
-        String expected = Endpoints.VALID_NAME_FOR_HEADER;
-        String responseBody = given()
-                .headers(headers)
-                .cookies(cookies)
-                .when()
-                .get(Endpoints.getSearchUrl(Endpoints.SEARCH_BY_VALID_NAME_FOR_HEADER))
-                .then()
-                .statusCode(200)
-                .extract()
-                .body()
-                .asString();
-        String actualText = ApiResponseParser.getTextFromResultHeader(responseBody);
-
-        assertThat(actualText, containsString(expected));
+        Steps.runSearchTest(Endpoints.SEARCH_BY_VALID_NAME_FOR_HEADER,
+                Endpoints.VALID_NAME_FOR_HEADER,
+                ApiResponseParser::getTextFromResultHeader);
     }
 
     @Test
     @DisplayName("Search by valid product name - test first product card")
     public void testSearchByValidProductNameFirstProductCard() {
-        String expected = Endpoints.VALID_NAME_FOR_CARD.toLowerCase();
-        String responseBody = given()
-                .headers(headers)
-                .cookies(cookies)
-                .when()
-                .get(Endpoints.getSearchUrl(Endpoints.SEARCH_BY_VALID_NAME_FOR_CARD))
-                .then()
-                .statusCode(200)
-                .extract()
-                .body()
-                .asString();
-        String actualText = ApiResponseParser.getTextFromFirstProductCard(responseBody).toLowerCase();
-
-        assertThat(actualText, containsString(expected));
+        Steps.runSearchTest(Endpoints.SEARCH_BY_VALID_NAME_FOR_CARD,
+                Endpoints.VALID_NAME_FOR_CARD.toLowerCase(),
+                response -> ApiResponseParser.getTextFromFirstProductCard(response).toLowerCase());
     }
 
     @AfterEach
-    public void tearsDown(TestInfo testInfo) {
+    public void tearDown(TestInfo testInfo) {
         logger.info("Test completed: {}", testInfo.getDisplayName());
     }
 }
