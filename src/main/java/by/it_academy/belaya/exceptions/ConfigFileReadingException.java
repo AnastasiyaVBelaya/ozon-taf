25a7e478ce7771ce7a0256685adbf63907d23104
Anastasiya Belaya <anastasia-belaya@mail.ru>
2025-03-13 21:48:17 +0300

    delete unused methods

--- src/main/java/by/it_academy/belaya/exceptions/ConfigFileReadingException.java
+++ src/main/java/by/it_academy/belaya/exceptions/ConfigFileReadingException.java
@@ -3,14 +3,6 @@ package by.it_academy.belaya.exceptions;
 public class ConfigFileReadingException extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "Error reading configuration file: ";
 
-    public ConfigFileReadingException() {
-        super(DEFAULT_MESSAGE);
-    }
-
-    public ConfigFileReadingException(String fileName) {
-        super(DEFAULT_MESSAGE + fileName);
-    }
-
     public ConfigFileReadingException(Throwable cause, String fileName) {
         super(DEFAULT_MESSAGE + fileName, cause);
     }
